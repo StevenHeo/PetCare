3e99140b5435e5e2394a83e6e6b1b038c0b134db
wjddms <wjddms@ajou.ac.kr>
2021-04-26 18:29:29 +0900

    test 커밋

--- app/src/main/java/com/jeongseok/petcare/MainActivity.java
+++ app/src/main/java/com/jeongseok/petcare/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button start_btn;
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
