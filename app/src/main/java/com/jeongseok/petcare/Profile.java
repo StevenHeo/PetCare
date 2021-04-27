7ac35600268796defb85efe18ce81b419da50336
허석민 <gjtjrals0@gmail.com>
2021-04-27 21:45:08 +0900

    Add HealthData.java, Profile.java, HomeActivity.java file
    ProfileActivity에 HomeActivity로 next intent 코드 추가
    HomeActivity를 위한 activity_home.xml 파일 추가

---
+++ app/src/main/java/com/jeongseok/petcare/Profile.java
@@ -0,0 +1,4 @@
+package com.jeongseok.petcare;
+
+public class Profile {
+}
--- app/src/main/java/com/jeongseok/petcare/ProfileActivity.java
+++ app/src/main/java/com/jeongseok/petcare/ProfileActivity.java
@@ -1,14 +1,18 @@
 package com.jeongseok.petcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
-        private ImageView back_image;
+    private Button next_btn;
+    private ImageView back_image;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,15 @@ public class ProfileActivity extends AppCompatActivity {
         back_image = (ImageView)findViewById(R.id.backbtn_image);
 
         backButton(back_image);
+
+        next_btn = (Button)findViewById(R.id.profile_button);
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next_intent = new Intent(ProfileActivity.this, HomeActivity.class);
+                startActivity(next_intent);
+            }
+        });
     }
     private void backButton(ImageView back_image){
         back_image.setOnClickListener(new View.OnClickListener() {
