a091aa76ae5636eb8f2b1ea8255b3afca91c502b
wjddms <wjddms@ajou.ac.kr>
2021-04-28 16:38:48 +0900

    Add: CalendarIn 관련
    Calendar in Activity popup 관련 코드 추가,
    popup menu.xml 추가,
    Calendar_in xml 코드 추가,
    Calendar_in 관련 리소스 추가

--- app/src/main/java/com/jeongseok/petcare/CalendarInActivity.java
+++ app/src/main/java/com/jeongseok/petcare/CalendarInActivity.java
@@ -3,12 +3,130 @@ package com.jeongseok.petcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 public class CalendarInActivity extends AppCompatActivity {
-
+    private ImageView back_image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_in);
+        back_image = (ImageView)findViewById(R.id.backbtn_image);
+
+        backButton(back_image);
+
+        //입관련
+        findViewById(R.id.mouth_down).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
+                getMenuInflater().inflate(R.menu.mouth_popup,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch( item.getItemId() ){
+                            case R.id.mouth_menu0:
+                                break;
+                            case R.id.mouth_menu1:
+                                break;
+                            case R.id.mouth_menu2:
+                                break;
+                            case R.id.mouth_menu3:
+                                break;
+                            case R.id.mouth_menu4:
+                                break;
+
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+        //피부관련
+        findViewById(R.id.skin_down).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
+                getMenuInflater().inflate(R.menu.skin_popup,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch( item.getItemId() ){
+                            case R.id.skin_menu0:
+                                break;
+                            case R.id.skin_menu1:
+                                break;
+                            case R.id.skin_menu2:
+                                break;
+                            case R.id.skin_menu3:
+                                break;
+                            case R.id.skin_menu4:
+                                break;
+                            case R.id.skin_menu5:
+                                break;
+                            case R.id.skin_menu6:
+                                break;
+                            case R.id.skin_menu7:
+                                break;
+                            case R.id.skin_menu8:
+                                break;
+                            case R.id.skin_menu9:
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+        //eye관련
+        findViewById(R.id.eye_down).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
+                getMenuInflater().inflate(R.menu.eye_popup,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch( item.getItemId() ){
+                            case R.id.eye_menu0:
+                                break;
+                            case R.id.eye_menu1:
+                                break;
+                            case R.id.eye_menu2:
+                                break;
+                            case R.id.eye_menu3:
+                                break;
+                            case R.id.eye_menu4:
+                                break;
+                            case R.id.eye_menu5:
+                                break;
+                            case R.id.eye_menu6:
+                                break;
+                            case R.id.eye_menu7:
+                                break;
+                            case R.id.eye_menu8:
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+
+    }
+
+    private void backButton(ImageView back_image){
+        back_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
