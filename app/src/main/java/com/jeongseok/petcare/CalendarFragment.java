afc677fcd5b69724e7d093b3103bcfe34a8fb68d
wjddms <wjddms@ajou.ac.kr>
2021-05-06 23:41:26 +0900

    Add select Date Event, calendar icon
    해당 날짜 클릭시 calendarIn 액티비티로 화면 전환,
    캘린더 아이콘 추가 (good_ic / normal_ic / bad_ic),
    calendarIn 액티비티 화면 전환시 editTextView로 포커싱 막기

--- app/src/main/java/com/jeongseok/petcare/CalendarFragment.java
+++ app/src/main/java/com/jeongseok/petcare/CalendarFragment.java
@@ -1,23 +1,47 @@
 package com.jeongseok.petcare;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.Executors;
+
 public class CalendarFragment extends Fragment {
     private Button button;
+    private View v;
+    MaterialCalendarView materialCalendarView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_calendar,container,false);
+        v = inflater.inflate(R.layout.fragment_calendar,container,false);
+        materialCalendarView = (MaterialCalendarView) v.findViewById(R.id.calendarView);
+        materialCalendarView.setSelectedDate(CalendarDay.today());
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Intent next_intent = new Intent(getActivity(),CalendarInActivity.class);
+                startActivity(next_intent);
+            }
+        });
 
         return v;
     }
