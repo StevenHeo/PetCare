1295dc3363ec83f9c8e484c7e4bfe3ddea1057b9
허석민 <gjtjrals0@gmail.com>
2021-05-26 23:14:39 +0900

    Fix text name at profile activity
    - set backpressed function

--- app/src/main/java/com/jeongseok/petcare/EditActivity.java
+++ app/src/main/java/com/jeongseok/petcare/EditActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +12,25 @@ import androidx.appcompat.app.AppCompatActivity;
 public class EditActivity extends AppCompatActivity {
 
     private Button editButton;
+    private ImageView backButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         editButton = (Button)findViewById(R.id.editSave_btn);
+        backButton = (ImageView)findViewById(R.id.backbtn_image);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent next_intent = new Intent(EditActivity.this,HomeActivity.class);
-                startActivity(next_intent);
+                onBackPressed();
             }
         });
     }
